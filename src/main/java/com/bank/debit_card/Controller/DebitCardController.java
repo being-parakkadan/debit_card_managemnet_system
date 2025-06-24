8c8d9df7b3c2c5d0b6fc2ec2de1841d5eae46c84
jerald <jeraldtom05@gmail.com>
2025-06-24 17:54:50 +0530

    added APIS for cardusage

--- src/main/java/com/bank/debit_card/Controller/DebitCardController.java
+++ src/main/java/com/bank/debit_card/Controller/DebitCardController.java
@@ -1,4 +1,25 @@
 package com.bank.debit_card.Controller;
 
+import com.bank.debit_card.Entity.DebitCardEntity;
+import com.bank.debit_card.Service.DebitCardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/debit_card")
 public class DebitCardController {
+
+    @Autowired
+    private DebitCardService debitCardService;
+
+    @GetMapping("/{cardId}")
+    public DebitCardEntity getCardUsage(@PathVariable String cardId){
+        return debitCardService.getCardUsage(cardId);
+    }
+
+    @PutMapping("/{cardId}")
+    public DebitCardEntity updateCardUsage(@PathVariable String cardId,@RequestBody DebitCardEntity updatedUsage){
+        return debitCardService.updateCardUsage(cardId,updatedUsage);
+    }
+
 }
