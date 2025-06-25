759bf553993737462f8d70008521bc818ae3338b
being-parakkadan <reshabparakkadan@gmail.com>
2025-06-26 00:11:27 +0530

    added global config for connection using ngrok

---
+++ src/main/java/com/bank/debit_card/Config/GlobalCorsConfig.java
@@ -0,0 +1,22 @@
+package com.bank.debit_card.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")  // Allow all origins (for now)
+                        .allowedMethods("*")   // Allow all HTTP methods
+                        .allowedHeaders("*");  // Allow all headers
+            }
+        };
+    }
+}
