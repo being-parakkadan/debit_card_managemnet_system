e5c379214224fdea6447e40881e9c00e704cf4e0
jerald <jeraldtom05@gmail.com>
2025-06-25 10:05:45 +0530

    renamed API

--- src/main/java/com/bank/debit_card/Controller/DebitCardController.java
+++ src/main/java/com/bank/debit_card/Controller/DebitCardController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/debit_card")
+@RequestMapping("/debit_card")
 public class DebitCardController {
 
     @Autowired
